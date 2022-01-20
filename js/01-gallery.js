4e99e38c014034c50fd147a5e7f6b543bdbfefe8
NataliiaChapai <natalya.chapay@gmail.com>
2022-01-20 18:31:37 +0200

    add solve of the task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,9 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryRef = document.querySelector('.gallery');
 const itemMakrup = createGalleryMarkup(galleryItems);
+
 galleryRef.insertAdjacentHTML('beforeend', itemMakrup);
+galleryRef.addEventListener('click', onGalleryItemClick);
 
 function createGalleryMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -20,6 +22,22 @@ function createGalleryMarkup(galleryItems) {
     })
         .join('');
 }
+
+function onGalleryItemClick(event) {
+    const isImageEl = event.target.classList.contains('gallery__image');
+    const imageSrc = event.target.dataset.source;
+    
+    event.preventDefault();
+    if (!isImageEl) {
+        return;
+    }
+    
+    const instance = basicLightbox.create(`
+    <img src="${imageSrc}" width="800" height="600">
+    `)
+    instance.show()
+}
+
 
 
 
